05f718549a1bf757a6fdd6ce7dea2fa80c0a6e1e
Sukhbir Singh <sukhbir.in@gmail.com>
2011-07-05 02:40:14 +0530

    Updated the listat.py and made it more modular to work with NNTP parsing

--- liststat.py
+++ liststat.py
@@ -12,7 +12,8 @@ is calculated using various metrics such as:
     and blank lines (individually).
 
 This script works for any mailing list that runs on GNU Mailman and where 
-Pipermail is used as the mail archiver. You just need to specify the list URL
+Pipermail is used as the mail archiver but it will parse a local mailbox also
+provided the right function is called. You just need to specify the list URL
 and the script will automatically download all the mbox archives, parse them
 and generate the data required. The data gathered is then stored in a database
 which you can query to get the specific statistics you desire.
@@ -30,7 +31,6 @@ import hashlib
 import urllib2
 import urlparse
 import datetime
-import collections
 import ConfigParser
 
 import psycopg2
@@ -60,13 +60,6 @@ LOG_FILE_PATH = os.path.join(LOG_PATH, LOG_FILE)
 
 DATABASE = {'name': 'teammetrics'}
             
-
-def is_root():
-    """Check if the user has root privileges."""
-    # If the user doesn't have root privileges return False. We plan to fix 
-    # this later by creating a teammetrics group.
-    return False if os.getuid() else True
-
 
 def write_checksum(hashes):
     """Write the hashes generated from the downloaded mbox archives."""
@@ -117,7 +110,7 @@ def get_current_month():
     return current_month
 
 
-def get_configuration():
+def get_configuration(config_file_path=CONF_FILE_PATH, pipermail=True):
     """Read the lists to be parsed from the configuration file.
  
     This function returns a mapping of list-name to list-addresses. In case
@@ -127,11 +120,11 @@ def get_configuration():
 
     config = ConfigParser.SafeConfigParser()
     try:
-        config.read(CONF_FILE_PATH)
+        config.read(config_file_path)
     except ConfigParser.Error as detail:
         logging.error(detail)
     # Get the names for the mailing lists from the config file.
-    sections = [section for section in config.sections()]
+    sections = config.sections()
 
     # Create a mapping of the list name to the address(es).
     mailing_list_parse = {}
@@ -140,18 +133,28 @@ def get_configuration():
         # In case the url and lists options are not found, skip the section.
         try:
             base_url = config.get(section, 'url')
-            list_ = config.get(section, 'lists').splitlines()
+            lists = config.get(section, 'lists').splitlines()
         except ConfigParser.NoOptionError as detail:
             logging.error(detail)
             continue
         if not base_url:
             logging.error('Base URL cannot be empty. Skipping list %s' % section)
             continue
-        if not list_:
+        if not lists:
             logging.error('List URL cannot be empty. Skipping list %s' % section)
             continue
+
+        # If pipermail is True, skip the URLs that don't have Pipermail in them.
+        # This is used to filter the lists that use NNTP for parsing. 
+        if pipermail:
+            if not 'pipermail' in base_url:
+                continue
+        else:
+            if 'pipermail' in base_url:
+                continue
+        
         # Just in case someone forgot to append the /.
-        for each_list in list_:
+        for each_list in lists:
             if base_url.endswith('/'):
                 mailing_list.append('{0}{1}'.format(base_url, each_list))
             else:
@@ -159,7 +162,15 @@ def get_configuration():
         # Mapping of list-name to list URL (or a list of list-URLs).
         mailing_list_parse[section] = mailing_list
 
-    return mailing_list_parse
+    # Calculate the total number of lists by iterating through the values of 
+    # mailing_list_parse. Note that there are multiple values per key. 
+    total_lists = len([item for items in mailing_list_parse.values() 
+                                                            for item in items])
+    if not total_lists:
+        logging.error('No lists found in configuration file')
+        sys.exit()
+    
+    return mailing_list_parse, total_lists
 
 
 def parse_and_save(mbox_files, mbox_hashes):
@@ -174,8 +185,8 @@ def parse_and_save(mbox_files, mbox_hashes):
     cur = conn.cursor()
 
     for url, files in mbox_files.iteritems():
-        mbox_ = mailbox.mbox(files)
-        for message in mbox_:
+        mbox_file = mailbox.mbox(files)
+        for message in mbox_file:
             # Name of the mailing list.
             mailing_list = os.path.basename(files).split('.')[0]
             project = mailing_list.rsplit('-', 2)[0]
@@ -299,7 +310,7 @@ def parse_and_save(mbox_files, mbox_hashes):
     sys.exit()
 
 
-def main(conf_info): 
+def main(conf_info, total_lists): 
     """Fetch the mbox archives from the URLs specified.
 
         mbox_files      -   mapping of URLs to the path of local mbox archives
@@ -307,13 +318,6 @@ def main(conf_info):
         conf_info       -   mapping of list names to their URLs
         mbox_archives   -   mbox archives in gzip format 
     """ 
-
-    # Calculate the total number of lists by iterating through the values of 
-    # conf_info. Note that there are multiple values per key. 
-    total_lists = len([item for items in conf_info.values() for item in items])
-    if not total_lists:
-        logging.error('No lists found to parse as no lists were found')
-        sys.exit()
 
     count = 0
     mbox_files = {}
@@ -325,82 +329,80 @@ def main(conf_info):
     # The conf_info has a mapping of {list-name : [list-url]}. So we go through
     # each value and download the corresponding list.
     for names, lists in conf_info.iteritems():
-        for name in names:
-            for list_ in lists:
-                logging.info('\tList %d of %d' % (count+1, total_lists))
-                logging.info("Reading %s" % list_)
+        for lst in lists:
+            logging.info('\tList %d of %d' % (count+1, total_lists))
+            logging.info("Reading %s" % lst)
+            try:
+                url = urllib2.urlopen(lst)
+            except (urllib2.URLError, ValueError) as detail:
+                logging.error("Unable to fetch mailing list"
+                                                " archive %s" % detail)
+                count += 1
+                continue                
+            response = url.read()
+
+            # Find all the <a> tags ending in '.txt.gz'.
+            soup = BeautifulSoup.BeautifulSoup(response)
+            parse_dates = soup.findAll('a', href=re.compile('\.txt\.gz$'))
+            # Extract the months from the <a> tags. This is used to download
+            # the mbox archive corresponding to the months the list was active.
+            archive_dates = []
+            archive_dates.extend([str(element.get('href')) for element 
+                                                            in parse_dates])
+
+            # Download all lists except that of the current month. 
+            if current_month in archive_dates:
+                archive_dates.remove(current_month)
+
+            # Skip if there are no dates.
+            if not archive_dates:
+                logging.warning('No dates found. Skipping')
+                count += 1
+                continue
+
+            # Download the mbox archives and save them to DIRECTORY_PATH.
+            logging.info("Downloading %d mbox archives" % len(archive_dates))
+            for date in archive_dates:
+                mbox_url = '{0}/{1}'.format(lst, date)
+                mbox_name = '{0}-{1}'.format(lst.split('/')[-1], date)
+                path_to_archive = os.path.join(ARCHIVES_FILE_PATH, mbox_name)
+                # Open the mbox archive and save it to the local disk.
                 try:
-                    url = urllib2.urlopen(list_)
-                except (urllib2.URLError, ValueError) as detail:
-                    logging.error("Unable to fetch mailing list"
-                                                    " archive %s" % detail)
-                    count += 1
-                    continue                
-                response = url.read()
-
-                # Find all the <a> tags ending in '.txt.gz'.
-                soup = BeautifulSoup.BeautifulSoup(response)
-                parse_dates = soup.findAll('a', href=re.compile('\.txt\.gz$'))
-                # Extract the months from the <a> tags. This is used to download
-                # the mbox archive corresponding to the months the list was active.
-                archive_dates = []
-                archive_dates.extend([str(element.get('href')) for element 
-                                                                in parse_dates])
-
-                # Download all lists except that of the current month. 
-                if current_month in archive_dates:
-                    archive_dates.remove(current_month)
-
-                # Skip if there are no dates.
-                if not archive_dates:
-                    logging.warning('No dates found. Skipping')
+                    mbox = urllib2.urlopen(mbox_url)
+                except urllib2.URLError as detail:
+                    logging.error('%s - %s' % (detail, mbox_url))
                     count += 1
                     continue
-
-                # Download the mbox archives and save them to DIRECTORY_PATH.
-                logging.info("Downloading %d mbox archives" % len(archive_dates))
-                for date in archive_dates:
-                    mbox_url = '{0}/{1}'.format(list_, date)
-                    mbox_name = '{0}-{1}'.format(list_.split('/')[-1], date)
-                    path_to_archive = os.path.join(ARCHIVES_FILE_PATH, mbox_name)
-                    # Open the mbox archive and save it to the local disk.
-                    try:
-                        mbox = urllib2.urlopen(mbox_url)
-                    except urllib2.URLError as detail:
-                        logging.error('%s - %s' % (detail, mbox_url))
-                        count += 1
+                with open(path_to_archive, 'wb') as f:
+                    mbox_archives.append(path_to_archive)
+                    f.write(mbox.read())
+            
+                # Get the SHA-1 hash of the current mbox. 
+                mbox_hash = generate_checksum(path_to_archive, mbox_name)
+                # If the SHA-1 from the already parsed mbox archives is 
+                # equal to the SHA-1 of the current mbox, don't parse the
+                # archive and remove it from mbox_archives.
+                mbox_hash_file = list(set(parsed_hash) & set(mbox_hash))
+                if mbox_hash_file:
+                    if parsed_hash[mbox_name] == mbox_hash[mbox_name]:
+                        logging.warning("Skipping already downloaded "
+                                           "and parsed mbox %s" % mbox_name)
+                        mbox_archives.remove(path_to_archive)
+                        os.remove(path_to_archive)
                         continue
-                    with open(path_to_archive, 'wb') as f:
-                        mbox_archives.append(path_to_archive)
-                        f.write(mbox.read())
-                
-                    # Get the SHA-1 hash of the current mbox. 
-                    mbox_hash = generate_checksum(path_to_archive, mbox_name)
-                    # If the SHA-1 from the already parsed mbox archives is 
-                    # equal to the SHA-1 of the current mbox, don't parse the
-                    # archive and remove it from mbox_archives.
-                    mbox_hash_file = list(set(parsed_hash) & set(mbox_hash))
-                    if mbox_hash_file:
-                        if parsed_hash[mbox_name] == mbox_hash[mbox_name]:
-                            logging.warning("Skipping already downloaded "
-                                               "and parsed mbox %s" % mbox_name)
-                            mbox_archives.remove(path_to_archive)
-                            os.remove(path_to_archive)
-                            continue
 
-                    # Extract the mbox file (plain text) from the gzip archive. 
-                    mbox_plain_text = '{0}'.format(mbox_name.rsplit('.', 1)[0])
-                    mbox_path = os.path.join(ARCHIVES_FILE_PATH, mbox_plain_text)
-                    with open(mbox_path, 'w') as gzip_file:
-                        temp_file = gzip.open(path_to_archive, 'rb')
-                        archive_contents = temp_file.read()
-                        gzip_file.write(archive_contents)
-                        mbox_files[mbox_url] = mbox_path
-                        # Update the hash for the archive downloaded.
-                        mbox_hashes.update(mbox_hash)
-                        logging.info('%s downloaded ' % mbox_name)
-                count += 1
-            break
+                # Extract the mbox file (plain text) from the gzip archive. 
+                mbox_plain_text = '{0}'.format(mbox_name.rsplit('.', 1)[0])
+                mbox_path = os.path.join(ARCHIVES_FILE_PATH, mbox_plain_text)
+                with open(mbox_path, 'w') as gzip_file:
+                    temp_file = gzip.open(path_to_archive, 'rb')
+                    archive_contents = temp_file.read()
+                    gzip_file.write(archive_contents)
+                    mbox_files[mbox_url] = mbox_path
+                    # Update the hash for the archive downloaded.
+                    mbox_hashes.update(mbox_hash)
+                    logging.info('%s downloaded ' % mbox_name)
+            count += 1
 
     # We don't need the mbox archives, so delete them.
     if mbox_archives:
@@ -417,10 +419,14 @@ def main(conf_info):
     parse_and_save(mbox_files, mbox_hashes)
 
 
+def start_logging():
+    """Initialize the logging."""
+    logging.basicConfig(filename=LOG_FILE_PATH,
+                        level=logging.INFO,
+                        format='%(asctime)s %(levelname)s: %(message)s')
+
+
 if __name__ == '__main__':
-    # Are we root?
-    if not is_root():
-        sys.exit('Please run this script with root privileges.')
     # Create the directories on first run. NOTE: This will be changed later. 
     if not os.path.isdir(LOG_PATH):
         os.mkdir(LOG_PATH)
@@ -428,19 +434,16 @@ if __name__ == '__main__':
         open(LOG_FILE_PATH, 'w').close()
 
     # Initialize the logging.
-    logging.basicConfig(filename=LOG_FILE_PATH,
-                        level=logging.INFO,
-                        format='%(asctime)s %(levelname)s: %(message)s')
+    start_logging()
     logging.info('\t\tStarting ListStat')
 
     # Get the configuration.
-    conf_info = get_configuration()
+    conf_info, total_lists = get_configuration()
     
     # Check whether the DATABASE dictionary is populated.
-    for keys, items in DATABASE.iteritems():
-        if not items:
-            logging.error('Please set a value for DATABASE %s' % keys)
-            sys.exit(1)
+    if not DATABASE['name']:
+        logging.error("Please set a value for 'name' key of DATABASE")
+        sys.exit(1)
     # Simulate a connection just to check whether everything is OK.
     try:
         psycopg2.connect(database=DATABASE['name'])
@@ -461,4 +464,4 @@ if __name__ == '__main__':
     if not os.path.isfile(HASH_FILE_PATH):
         open(HASH_FILE_PATH, 'w').close()
 
-    main(conf_info)
+    main(conf_info, total_lists)
