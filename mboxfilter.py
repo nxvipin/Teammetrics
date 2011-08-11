5ebfae264ecc01f702f3267a65bfdfe7b3d45c5b
Andreas Tille <tille@debian.org>
2011-08-11 14:24:45 +0200

    Rewrote header parsing to enable easier adding some other fields of interest

--- mboxfilter.py
+++ mboxfilter.py
@@ -6,21 +6,19 @@ import mailbox
 
 import nntpstat
 
+HEADERS = ['From', 'Date', 'Subject', 'Message-ID', 'In-Reply-To', 'References']
 
 def main(mbox_file):
     mbox = mailbox.mbox(mbox_file)
 
-    from_field = []
-    date = []
-    subject = []
-    msg_id = []
     content = []
+    header  = []
 
     for message in mbox:
-        from_field.append(message['From'])
-        date.append(message['Date'])
-        subject.append(message['Subject'])
-        msg_id.append(message['Message-Id'])
+	tmpheader = {}
+	for h in HEADERS:
+	    tmpheader[h] = message[h]
+	header.append(tmpheader)
         raw_content = message.get_payload()
         if isinstance(raw_content, list):
             content.append(raw_content[0].get_payload())
@@ -29,23 +27,20 @@ def main(mbox_file):
 
     mbox.close()
 
-    mbox_format = """From {0}
-From: {1}
-Date: {2}
-Subject: {3}
-Message-ID: {4}
-
-"""
-
     with open(mbox_file, 'w') as mbox_f:
-        for f, d, s, m, c in zip(from_field, date, subject, msg_id, content):
-
-            email, name = nntpstat.format_mail_name(f)
-            updated_date = nntpstat.asctime_update(d, m)
-            f_one = '{0}  {1}'.format(email, updated_date)
-            f_two = '{0} ({1})'.format(email, name)
+        for h, c in zip(header, content):
+            email, name = nntpstat.format_mail_name(h['From'])
+            updated_date = nntpstat.asctime_update(h['Date'], h['Message-ID'])
+            h['From0'] = '{0}  {1}'.format(email, updated_date)
+            h['From']  = '{0} ({1})'.format(email, name)
           
-            mbox_f.write(mbox_format.format(f_one, f_two, d, s, m)) 
+	    mbox_format = "From %(From0)s\n"
+	    for hx in HEADERS:
+		if h.has_key(hx) and h[hx] != None:
+		    mbox_format = mbox_format + hx + ": %(" + hx + ")s\n"
+	    mbox_format = mbox_format + "\n"
+
+            mbox_f.write(mbox_format % (h)) 
             try:
                 mbox_f.writelines(c)
             except TypeError:
