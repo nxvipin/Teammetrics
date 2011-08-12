4e941ff8e3501f98e60ab14d5d27c03f568a9fbb
Andreas Tille <tille@debian.org>
2011-08-12 20:40:43 +0200

    Seems we have found the most promising way for the mbox filter, just get rid of the non-working ones

--- mboxfilter.py
+++ mboxfilter.py
@@ -1,54 +1,37 @@
 #! /usr/bin/env python
 
-import os
 import sys
 import mailbox
 
-import nntpstat
+HEADERS = ('From', 'Date', 'Subject', 'Message-ID', 'In-Reply-To', 'References')
 
-HEADERS = ['From', 'Date', 'Subject', 'Message-ID', 'In-Reply-To', 'References']
 
 def main(mbox_file):
-    mbox = mailbox.mbox(mbox_file)
 
-    content = []
-    header  = []
+    out_mbox = mbox_file + '.converted'
+    mbox = open(out_mbox, 'w')
 
-    for message in mbox:
-	tmpheader = {}
-	for h in HEADERS:
-	    tmpheader[h] = message[h]
-	header.append(tmpheader)
-        raw_content = message.get_payload()
-        if isinstance(raw_content, list):
-            content.append(raw_content[0].get_payload())
-        else:
-            content.append(raw_content)
+    with open(mbox_file) as f:
+        stop = False
+        first_add = True
+        for line in f:
 
-    mbox.close()
+            if line.startswith(HEADERS):
+                stop = False
+                first_add = True
 
-    with open(mbox_file, 'w') as mbox_f:
-        for h, c in zip(header, content):
-            email, name = nntpstat.format_mail_name(h['From'])
-            updated_date = nntpstat.asctime_update(h['Date'], h['Message-ID'])
-            h['From0'] = '{0}  {1}'.format(email, updated_date)
-            h['From']  = '{0} ({1})'.format(email, name)
-          
-	    mbox_format = "From %(From0)s\n"
-	    for hx in HEADERS:
-		if h.has_key(hx) and h[hx] != None:
-		    mbox_format = mbox_format + hx + ": %(" + hx + ")s\n"
-	    mbox_format = mbox_format + "\n"
+                print >>mbox, line,
 
-            mbox_f.write(mbox_format % (h)) 
-            try:
-                mbox_f.writelines(c)
-            except TypeError, err:
-                print str(h['Message-ID']) + "\n" + str(err) + "\n" + str(c)
+            if stop:
+                if first_add:
+                    print >>mbox
+                    first_add = False
 
-            mbox_f.write('\n')
-
-    sys.exit()
+                print >>mbox, line,
+            
+            if line == '\n':
+                stop = True
+            
 
 
 if __name__ == '__main__':
