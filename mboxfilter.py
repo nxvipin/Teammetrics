f85cf8d263237c05609e12cdb1cfb7a20aa7a445
Sukhbir Singh <sukhbir.in@gmail.com>
2011-08-15 15:50:08 +0530

    Added support for deleting messages matching specific Message-IDs

--- mboxfilter.py
+++ mboxfilter.py
@@ -7,6 +7,7 @@ HEADERS = ('From',
            'Date',
            'Subject',
            'Message-ID',
+           'Message-Id',
            'In-Reply-To',
            'References',
            'Content-Type',
@@ -14,8 +15,7 @@ HEADERS = ('From',
            'Content-Transfer-Encoding',
            'X-Spam-Status',
            'X-Debian-PR-Package',
-           'X-Debian-PR-Keywords',
-          )
+           'X-Debian-PR-Keywords',)
 
 
 def main(mbox_file):
@@ -44,6 +44,33 @@ def main(mbox_file):
             if line == '\n':
                 stop = True
             
+    print 'Headers stripped from mbox'
+
+    # Now open the converted mbox archive to delete messages with the
+    # Message-IDs specified in the file messageid.
+    msg_ids = []
+    try:
+        with open('messageid') as f:
+            msg_ids = [line.strip() for line in f.readlines()]
+    except IOError:
+        sys.exit('Error: messageid file not found in current working directory')
+
+    mbox = mailbox.mbox(out_mbox)
+
+    mbox.lock()
+    counter = 0
+
+    for key, msg in mbox.iteritems():
+        msg_id = msg['Message-ID']
+        if msg_id in msg_ids: 
+            mbox.remove(key)
+            counter += 1
+
+    print 'Deleted %d messages' % counter
+
+    mbox.flush()
+    mbox.close()
+    sys.exit('Done')
 
 
 if __name__ == '__main__':
