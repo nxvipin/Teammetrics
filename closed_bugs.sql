fb8332688340e3238afbe4a4c08a797d1c08e482
Andreas Tille <tille@debian.org>
2011-12-18 01:30:56 +0100

    Function bug_closer_per_year_of_pkggroup is new (other functions need to be changed but I just left the template)

---
+++ closed_bugs.sql
@@ -0,0 +1,52 @@
+-- top 10 maintainers closing bugs in team packages
+CREATE OR REPLACE FUNCTION bug_closer_per_year_of_pkggroup(text,int) RETURNS SETOF RECORD AS $$
+  SELECT cn.name, uh.year, COUNT(*)::int FROM
+--    (SELECT source, EXTRACT(year FROM done_date)::int AS year, done_email   -- done_date is always 1970-01-01
+    (SELECT id, source, EXTRACT(year FROM last_modified)::int AS year, done_email FROM archived_bugs
+     UNION
+     SELECT id, source, EXTRACT(year FROM last_modified)::int AS year, done_email FROM bugs WHERE status = 'done'
+    ) uh
+    JOIN carnivore_emails ce ON ce.email = uh.done_email
+    JOIN (SELECT * FROM carnivore_names
+           WHERE id IN (SELECT idupl FROM active_uploader_ids_of_pkggroup($1, $2) AS (idupl int, count int))
+    )  cn ON ce.id    = cn.id
+    JOIN carnivore_names_prefered cnp ON cn.id = cnp.id
+   WHERE source IN (    -- source packages that are maintained by the team
+       SELECT DISTINCT source FROM upload_history
+        WHERE maintainer_email = $1
+          AND nmu = 'f'
+     )
+     AND done_email IN ( -- email of uploaders who at least once uploaded on behalf of the team
+      SELECT DISTINCT ce.email FROM upload_history uh
+        JOIN carnivore_emails ce ON ce.email = uh.changed_by_email
+       WHERE maintainer_email = $1
+     )
+     AND cn.name = cnp.name
+   GROUP BY cn.name, uh.year
+   ORDER BY year, count DESC, cn.name
+$$ LANGUAGE 'SQL';
+
+/*
+SELECT * FROM bug_closer_per_year_of_pkggroup('debian-med-packaging@lists.alioth.debian.org',50) AS (name text, year int, count int);
+SELECT * FROM bug_closer_per_year_of_pkggroup('debian-science-maintainers@lists.alioth.debian.org',50) AS (name text, year int, count int);
+*/
+
+-- top 10 maintainers as (hopefully!!!) unique name
+CREATE OR REPLACE FUNCTION active_uploader_names_of_pkggroup(text, int) RETURNS SETOF RECORD AS $$
+  SELECT cnp.name FROM
+    (SELECT id FROM active_uploader_ids_of_pkggroup($1, $2) AS (id int, count int)) au
+    JOIN carnivore_names_prefered cnp ON au.id = cnp.id
+$$ LANGUAGE 'SQL';
+
+/*
+SELECT * FROM active_uploader_names_of_pkggroup('debian-med-packaging@lists.alioth.debian.org',50) AS (name text);
+SELECT * FROM active_uploader_names_of_pkggroup('debian-science-maintainers@lists.alioth.debian.org',50) AS (name text);
+*/
+
+/*
+ Finally you need to
+
+   psql udd < /usr/share/postgresql/<pgversion>/contrib/tablefunc.sql
+
+ to be able to run uploader statistics script
+ */
