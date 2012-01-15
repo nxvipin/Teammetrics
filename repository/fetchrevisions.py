0e27e0bffe02f437f7ade932fa502b0c16514460
Sukhbir Singh <sukhbir.in@gmail.com>
2012-01-15 14:10:29 +0530

    Parse each team at a time for svnstat.py instead of all teams

--- repository/fetchrevisions.py
+++ repository/fetchrevisions.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import os
+import sys
 import shlex
 import datetime
 import subprocess
@@ -9,7 +10,6 @@ import collections
 from xml.etree import ElementTree as ET
 
 ALIOTH_PATH = '/srv/home/groups/teammetrics'
-TEAM_FILE = os.path.join(ALIOTH_PATH, 'teams.list')
 REVISION_FILE = os.path.join(ALIOTH_PATH, 'revisions.hash')
 PARSE_INFO_FILE = os.path.join(ALIOTH_PATH, 'parse.info')
 
@@ -28,79 +28,77 @@ def get_revisions():
 
 
 def parse_revision():
-    """Fetch the revisions for teams in TEAM_FILE."""
+    """Fetch the revisions for the called teams."""
     revisions = collections.defaultdict(list)
     done_revisions = get_revisions()
     today_date = datetime.date.today()
 
-    teams = []
-    with open(TEAM_FILE) as f:
-        teams = [line.strip() for line in f.readlines()]
-    
+    team = sys.argv[1]
     parse_f = open(PARSE_INFO_FILE, 'w')
     revision_f = open(REVISION_FILE, 'a')
-    for team in teams:
-        cmd_raw = 'svn log --xml file:///svn/{0}/'.format(team)
-        cmd = shlex.split(cmd_raw)
 
-        output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
-        output_xml = ET.fromstring(output)
+    cmd_raw = 'svn log --xml file:///svn/{0}/'.format(team)
+    cmd = shlex.split(cmd_raw)
 
-        # Get the list of committers and their revisions from the repository.
-        new_changes = []
-        author_info = collections.defaultdict(list)
-        revision_date = {}
-        for info in output_xml.getiterator('logentry'):
-            # In some cases, the author tag is missing.
-            try:
-                author, date, msg = [element.text for element in info.getchildren()]
-            except ValueError:
+    output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+    output_xml = ET.fromstring(output)
+
+    # Get the list of committers and their revisions from the repository.
+    new_changes = []
+    author_info = collections.defaultdict(list)
+    revision_date = {}
+    for info in output_xml.getiterator('logentry'):
+        # In some cases, the author tag is missing.
+        try:
+            author, date, msg = [element.text for element in info.getchildren()]
+        except ValueError:
+            continue
+        revision = int(info.get('revision'))
+        author_info[author].append(revision)
+        revision_date[revision] = date.split('T')[0]
+
+    total_authors = len(author_info)
+    for committer, revision in author_info.iteritems():
+        project = team
+        package = team
+        author = committer
+
+        # Fetch the diff for each revision of an author. If the revision
+        # has already been downloaded, it won't be downloaded again.
+        for change in revision:
+            # Open the REVISION_FILE_PATH that is used to save the parsed revisions.
+            inserted = 0
+            deleted = 0
+
+            change_format = '{0}:{1}'.format(project, change)
+            if change_format in done_revisions:
                 continue
-            revision = int(info.get('revision'))
-            author_info[author].append(revision)
-            revision_date[revision] = date.split('T')[0]
 
-        total_authors = len(author_info)
-        for committer, revision in author_info.iteritems():
-            project = team
-            package = team
-            author = committer
+            cmd_raw = 'svn diff -c {0} file:///svn/{1}/'.format(change, team) 
+            cmd = shlex.split(cmd_raw)
+            output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
 
-            # Fetch the diff for each revision of an author. If the revision
-            # has already been downloaded, it won't be downloaded again.
-            for change in revision:
-                # Open the REVISION_FILE_PATH that is used to save the parsed revisions.
-                inserted = 0
-                deleted = 0
+            lines = [line for line in output.splitlines() 
+                                                    if line.startswith(('+', '-'))]
+            for line in lines:
+                if not line.startswith(('+++', '---')):
+                    if line.startswith('+'):
+                        inserted += 1
+                    else:
+                        deleted += 1
 
-                change_format = '{0}:{1}'.format(project, change)
-                if change_format in done_revisions:
-                    continue
+            parse_f.write(FORMAT.format(change, project, package, 
+                                        author, revision_date[change],
+                                        today_date, inserted, deleted))
+            parse_f.write('\n')
 
-                cmd_raw = 'svn diff -c {0} file:///svn/{1}/'.format(change, team) 
-                cmd = shlex.split(cmd_raw)
-                output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
-
-                lines = [line for line in output.splitlines() 
-                                                        if line.startswith(('+', '-'))]
-                for line in lines:
-                    if not line.startswith(('+++', '---')):
-                        if line.startswith('+'):
-                            inserted += 1
-                        else:
-                            deleted += 1
-
-                parse_f.write(FORMAT.format(change, project, package, 
-                                            author, revision_date[change],
-                                            today_date, inserted, deleted))
-                parse_f.write('\n')
-
-                revision_f.write(change_format)
-                revision_f.write('\n')
-                parse_f.flush()
-                revision_f.flush()
+            revision_f.write(change_format)
+            revision_f.write('\n')
+            parse_f.flush()
+            revision_f.flush()
 
     parse_f.close()
     revision_f.close()
+    sys.exit()
 
 parse_revision()
--- repository/svnstat.py
+++ repository/svnstat.py
@@ -25,44 +25,37 @@ ALIOTH_PATH = '/srv/home/groups/teammetrics'
 def fetch_logs(ssh, conn, cur, teams):
     """Fetch and save the logs for SVN repositories."""
 
-    with open('teams.list', 'w') as f:
-        f.write('\n'.join(teams))
-
-    # Create a SFTPClient object to transfer the team list and revisions.
-    ftp = ssh.open_sftp()
-    ftp.chdir(ALIOTH_PATH)
-    ftp.put('teams.list', 'teams.list')
-
     logging.info('Parsing repository data')
-    cmd = 'python {0}/fetchrevisions.py'.format(ALIOTH_PATH)
-    stdin, stdout, stderr = ssh.exec_command(cmd)
-    output = stdout.read()
 
-    logging.info('Fetching parse info from vasks.debian.org ...')
-    ftp.get('parse.info', 'parse.info')
+    for team in teams:
+        cmd = 'python {0}/fetchrevisions.py {1}'.format(ALIOTH_PATH, team)
+        stdin, stdout, stderr = ssh.exec_command(cmd)
+        output = stdout.read()
 
-    parse_info = []
-    with open('parse.info') as f:
-        for line in f:
-            rev, project, package, author, rev_date, parse_date, inserted, deleted = line.split(',')
-            try:
-                cur.execute(
-            """INSERT INTO commitstat(commit_id, project, package, vcs, name, 
-                commit_date, today_date, lines_inserted, lines_deleted) 
-                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);""",
-                        (rev, project, package, 'svn', author, 
-                    rev_date, parse_date, inserted, deleted)
-                            )
-                conn.commit()
-            except psycopg2.DataError as detail:
-                conn.rollback()
-                logging.error(detail)
-                continue
-            except psycopg2.IntegrityError as detail:
-                conn.rollback()
-                logging.error('%s: project: %s, revision #: %s' % (detail, 
-                                                                project, 
-                                                                rev))
-                continue
+        logging.info('Fetching parse info from vasks.debian.org ...')
+        ftp.get('parse.info', 'parse.info')
+
+        with open('parse.info') as f:
+            for line in f:
+                rev, project, package, author, rev_date, parse_date, inserted, deleted = line.split(',')
+                try:
+                    cur.execute(
+                """INSERT INTO commitstat(commit_id, project, package, vcs, name, 
+                    commit_date, today_date, lines_inserted, lines_deleted) 
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);""",
+                            (rev, project, package, 'svn', author, 
+                        rev_date, parse_date, inserted, deleted)
+                                )
+                    conn.commit()
+                except psycopg2.DataError as detail:
+                    conn.rollback()
+                    logging.error(detail)
+                    continue
+                except psycopg2.IntegrityError as detail:
+                    conn.rollback()
+                    logging.error('%s: project: %s, revision #: %s' % (detail, 
+                                                                    project, 
+                                                                    rev))
+                    continue
 
     logging.info('SVN logs saved...')
