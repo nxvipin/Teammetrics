8327aa435d12c6474f534f88dcd9e2d74eaf408c
Vipin Nair <swvist@gmail.com>
2012-05-27 04:48:28 +0530

    Library to deal with metrics config file

---
+++ web/lib/metrics.py
@@ -0,0 +1,30 @@
+import ConfigParser
+import sys
+import os
+import logging
+
+cp = ConfigParser.ConfigParser()
+
+"""
+This is a temporary solution.
+Config file will be read from Django settings.
+"""
+FILE = '../config/metrics.conf'
+CONF_FILE = os.path.join(os.path.dirname(sys.argv[0]),FILE)
+
+try:
+	cp.readfp(open(CONF_FILE))
+except IOError:
+	logging.error("Config File Not Found")
+else:
+	def getMetric(metricname, listname):
+		try:
+			mlist = cp.get(metricname,listname).split(' ')
+		except ConfigParser.NoSectionError:
+			logging.error("No Such Team")
+			return []
+		except ConfigParser.NoOptionError:
+			logging.error("No Such Metric")
+			return []
+		else:
+			return mlist
