72bcedb5d330b1167ad7943375b622cc860e556b
Vipin Nair <swvist@gmail.com>
2012-06-09 22:51:33 +0530

    API endpoint that returns monthly data for a given team and metric

--- web/api/urls.py
+++ web/api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^v(?P<api_version>\d+)/index/$', 'api.views.index')
+    url(r'^v(?P<api_version>\d+)/(?P<team>[a-zA-Z-]+)/(?P<metric>[a-z]+)/$', 'api.views.month')
 )
