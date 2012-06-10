b5ea4e1f1fb5018357b9064b7d1a04e686949953
Vipin Nair <swvist@gmail.com>
2012-06-10 15:08:38 +0530

    Refactored existing code. Added function to handle monthly commit data.

--- web/api/views.py
+++ web/api/views.py
@@ -1,22 +1,20 @@
 from django.http import HttpResponse
-from web.models import listarchives
+from web.models import listarchives, commitstat
 from web.lib import metrics
-
+from web.lib import log
 import json
+
+logger = log.get(__name__)
 
 def _checkKeyExists(dlist, key, value):
     for d in dlist:
         if d[key]==value:
-            print dlist.index(d)
             return dlist.index(d)
     return -1
 
-def _monthList(team, mlist):
-    mdata=listarchives.month(mlist)
-    data=dict()
-    data['mailing-list'] = mlist
+def _processMonthlyData(data, dbdata):
     data['data'] = []
-    for i in mdata:
+    for i in dbdata:
         d = _checkKeyExists(data['data'],'year',int(float(i[0])))
         if d == -1 :
             data['data'].append({'year':int(float(i[0]))})
@@ -26,13 +24,28 @@ def _monthList(team, mlist):
         data['data'][d]['data'].append({'month':int(float(i[1])),'data':int(float(i[2]))})
     return data
 
+def _monthList(team, mlist):
+    dbdata=listarchives.month(mlist)
+    data=dict()
+    data['mailing-list'] = mlist
+    return _processMonthlyData(data, dbdata)
+
+def _monthCommits(team, repo):
+    dbdata=commitstat.month(repo)
+    data=dict()
+    data['repository'] = repo
+    return _processMonthlyData(data, dbdata)
+
 def month(request, api_version, team, metric):
     """
     Returns monthly data as JSON for a given team and metric.
     """
-    mlist = metrics.get(team,metric)
+    logger.info("Month Function Called")
+    metricname = metrics.get(team,metric)
     data = {'team' : team}
     data['data'] = {'metric' : metric}
     if metric == 'list':
-        data['data']['data'] = [_monthList(team,m) for m in mlist]
+        data['data']['data'] = [_monthList(team,m) for m in metricname]
+    elif metric == 'repository':
+        data['data']['data'] = [_monthCommits(team,m) for m in metricname]
     return HttpResponse(json.dumps(data))
