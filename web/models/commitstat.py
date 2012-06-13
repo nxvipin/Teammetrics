b65827827f6a54900fd52520ec261938ee49a6f3
Vipin Nair <swvist@gmail.com>
2012-06-13 21:07:11 +0530

    Fixed bugs and added Top 'N' functions

--- web/models/commitstat.py
+++ web/models/commitstat.py
@@ -15,7 +15,7 @@ def monthData(team):
                 WHERE project='%s'
                 GROUP BY YEAR, MONTH
                 ORDER BY YEAR; """
-    cur.execute(sql,(team,team))
+    cur.execute(sql,(team))
     return cur.fetchall()
 
 def annualData(team):
@@ -29,8 +29,47 @@ def annualData(team):
                 WHERE project='%s'
                 GROUP BY YEAR
                 ORDER BY YEAR; """
-    cur.execute(sql,(team,team))
+    cur.execute(sql,(team))
     return cur.fetchall()
 
+def monthTopN(team, n):
+    """
+    Returns monthly liststat data for Top 'N' members a given team.
+    """
+    sql = """   SELECT extract(YEAR
+                               FROM commit_date) AS YEAR,
+                       extract(MONTH
+                               FROM commit_date) AS MONTH,
+                       name,
+                       count(*)
+                FROM commitstat
+                WHERE project='%s'
+                    AND name IN (
+                    SELECT name
+                    FROM commitstat WHERE project = '%s'
+                GROUP BY name
+                ORDER BY count(*) DESC LIMIT %d)
+                GROUP BY YEAR, MONTH, name
+                ORDER BY YEAR, MONTH, COUNT DESC; """
+    cur.execute(sql,(team, team, n))
+    return cur.fetchall()
 
-
+def annualTopN(team, n):
+    """
+    Returns annual liststat data for Top 'N' members a given team.
+    """
+    sql = """   SELECT extract(YEAR
+                               FROM commit_date) AS YEAR,
+                       name,
+                       count(*)
+                FROM commitstat
+                WHERE project='%s'
+                    AND name IN (
+                    SELECT name
+                    FROM commitstat WHERE project = '%s'
+                GROUP BY name
+                ORDER BY count(*) DESC LIMIT %d)
+                GROUP BY YEAR, name
+                ORDER BY YEAR, COUNT DESC; """
+    cur.execute(sql,(team, team, n))
+    return cur.fetchall()
