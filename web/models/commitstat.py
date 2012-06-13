cddbc25f653b2c63341c6d896ec244761847088e
Vipin Nair <swvist@gmail.com>
2012-06-13 20:42:32 +0530

    Renamed the function and optimized the query

--- web/models/commitstat.py
+++ web/models/commitstat.py
@@ -1,29 +1,20 @@
 from web.models import database
+
 cur = database.connect()
-def month(team):
+
+def monthData(team):
     """
     Returns monthly liststat data for a given team.
     """
-    sql = """ select extract(year from commit_date) as year, 
-                    extract(month from commit_date) as month, 
-                    count(*) 
-                from 
-                    commitstat 
-                where 
-                    extract(year from commit_date) in 
-                      (select 
-                            distinct extract(year from commit_date) 
-                        from 
-                            commitstat 
-                        where 
-                           project = %s) 
-                and 
-                    project=%s 
-                group by 
-                    year, 
-                    month 
-                order by 
-                    year;"""
+    sql = """   SELECT extract(YEAR
+                               FROM commit_date) AS YEAR,
+                       extract(MONTH
+                               FROM commit_date) AS MONTH,
+                       count(*)
+                FROM commitstat
+                WHERE project='teammetrics'
+                GROUP BY YEAR, MONTH
+                ORDER BY YEAR; """
     cur.execute(sql,(team,team))
     return cur.fetchall()
 
