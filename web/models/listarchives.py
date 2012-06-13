384bf6626dcf6064b331e924f725553d383cbf6c
Vipin Nair <swvist@gmail.com>
2012-06-13 20:41:55 +0530

    Added function to fetch monthly liststat data of top N members.

--- web/models/listarchives.py
+++ web/models/listarchives.py
@@ -32,6 +32,29 @@ def annualData(team):
     cur.execute(sql,(team))
     return cur.fetchall()
 
+def monthTopN(team, n):
+    """
+    Return monthly liststat data of all time top 'N' members.
+    """
+    sql = """   SELECT extract(YEAR
+                               FROM archive_date) AS YEAR,
+                       extract(MONTH
+                               FROM archive_date) AS MONTH,
+                       name,
+                       count(*)
+                FROM listarchives
+                WHERE project='%s'
+                    AND name IN
+                        (SELECT name
+                         FROM listarchives
+                         WHERE project = '%s'
+                         GROUP BY name
+                         ORDER BY count(*) DESC LIMIT %d)
+                GROUP BY YEAR,MONTH, name
+                ORDER BY YEAR, MONTH, COUNT DESC; """
+    cur.execute(sql,(team, team, n))
+    return cur.fetchall()
+
 def annualTopN(team, n):
     """
     Returns annual liststat data for top 'N' members of a given team.
