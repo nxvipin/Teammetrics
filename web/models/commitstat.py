32707939790133e603b7ff4e746655e8cf977ae0
Vipin Nair <swvist@gmail.com>
2012-06-13 20:51:23 +0530

    Added function to return annual data for a team

--- web/models/commitstat.py
+++ web/models/commitstat.py
@@ -18,6 +18,19 @@ def monthData(team):
     cur.execute(sql,(team,team))
     return cur.fetchall()
 
+def annualData(team):
+    """
+    Returns monthly liststat data for a given team.
+    """
+    sql = """   SELECT extract(YEAR
+                               FROM commit_date) AS YEAR,
+                       count(*)
+                FROM commitstat
+                WHERE project='%s'
+                GROUP BY YEAR
+                ORDER BY YEAR; """
+    cur.execute(sql,(team,team))
+    return cur.fetchall()
 
 
 
