f9de8fe818cfc2b7c308ccc758dfcb4e686abd55
Vipin Nair <swvist@gmail.com>
2012-06-17 01:37:33 +0530

    Refactored code

--- web/api/views.py
+++ web/api/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from web.models import listarchives, commitstat
+from web.models import listarchives, commitstat, commitlines
 from web.lib import metrics
 from web.lib import log
 import json
@@ -25,27 +25,46 @@ def _processMonthlyData(data, dbdata):
     return data
 
 def _monthList(team, mlist):
-    dbdata=listarchives.month(mlist)
+    dbdata=listarchives.monthData(mlist)
     data=dict()
     data['mailing-list'] = mlist
     return _processMonthlyData(data, dbdata)
 
 def _monthCommits(team, repo):
-    dbdata=commitstat.month(repo)
+    dbdata=commitstat.monthData(repo)
     data=dict()
     data['repository'] = repo
     return _processMonthlyData(data, dbdata)
+
+def _monthCommitLines(team, repo):
+    dbdata=commitlines.monthData(repo)
+    data=dict()
+    data['repository'] = repo
+    return _processMonthlyData(data, dbdata)
+
+def _identifyMetric(team, metric):
+    if metric == 'list':
+        return metrics.get(team,'list')
+    elif metric == 'commits':
+        return metrics.get(team,'repository')
+    elif metric == 'commitlines':
+        return metrics.get(team,'repository')
+    else:
+        return None
 
 def month(request, api_version, team, metric):
     """
     Returns monthly data as JSON for a given team and metric.
     """
     logger.info("Month Function Called")
-    metricname = metrics.get(team,metric)
+    metricname = _identifyMetric(team, metric)
     data = {'team' : team}
     data['data'] = {'metric' : metric}
     if metric == 'list':
         data['data']['data'] = [_monthList(team,m) for m in metricname]
-    elif metric == 'repository':
+    elif metric == 'commits':
         data['data']['data'] = [_monthCommits(team,m) for m in metricname]
+    elif metric == 'commitlines':
+        data['data']['data'] = [_monthCommitLines(team,m) for m in metricname]
+        
     return HttpResponse(json.dumps(data))
