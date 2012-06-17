034d48d21de97644217c5da1eef57011953a7697
Vipin Nair <swvist@gmail.com>
2012-06-18 00:56:56 +0530

    Changed views to utilize helper functions

--- web/api/views.py
+++ web/api/views.py
@@ -2,62 +2,35 @@ from django.http import HttpResponse
 from web.models import listarchives, commitstat, commitlines
 from web.lib import metrics
 from web.lib import log
+from web.api import helper
 import json
 
 logger = log.get(__name__)
-
-def _checkKeyExists(dlist, key, value):
-    for d in dlist:
-        if d[key]==value:
-            return dlist.index(d)
-    return -1
-
-def _processMonthlyData(data, dbdata):
-    data['data'] = []
-    for i in dbdata:
-        d = _checkKeyExists(data['data'],'year',int(float(i[0])))
-        if d == -1 :
-            data['data'].append({'year':int(float(i[0]))})
-            d=len(data['data'])-1
-        if not data['data'][d].has_key('data'):
-            data['data'][d]['data']=[]
-        data['data'][d]['data'].append({'month':int(float(i[1])),'data':int(float(i[2]))})
-    return data
 
 def _monthList(team, mlist):
     dbdata=listarchives.monthData(mlist)
     data=dict()
     data['mailing-list'] = mlist
-    return _processMonthlyData(data, dbdata)
+    return helper.processMonthlyData(data, dbdata, ['liststat'])
 
 def _monthCommits(team, repo):
     dbdata=commitstat.monthData(repo)
     data=dict()
     data['repository'] = repo
-    return _processMonthlyData(data, dbdata)
+    return helper.processMonthlyData(data, dbdata, ['commits'])
 
 def _monthCommitLines(team, repo):
     dbdata=commitlines.monthData(repo)
     data=dict()
     data['repository'] = repo
-    return _processMonthlyData(data, dbdata)
-
-def _identifyMetric(team, metric):
-    if metric == 'list':
-        return metrics.get(team,'list')
-    elif metric == 'commits':
-        return metrics.get(team,'repository')
-    elif metric == 'commitlines':
-        return metrics.get(team,'repository')
-    else:
-        return None
+    return helper.processMonthlyData(data, dbdata, ['lines_added', 'lines_removed'])
 
 def month(request, api_version, team, metric):
     """
     Returns monthly data as JSON for a given team and metric.
     """
     logger.info("Month Function Called")
-    metricname = _identifyMetric(team, metric)
+    metricname = helper.identifyMetric(team, metric)
     data = {'team' : team}
     data['data'] = {'metric' : metric}
     if metric == 'list':
