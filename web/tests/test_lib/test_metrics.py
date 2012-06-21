ce13a5ff2480d06d097455c68b18555a789a24dd
Vipin Nair <swvist@gmail.com>
2012-06-21 07:15:41 +0530

    Added a test to detect missing config file

--- web/tests/test_lib/test_metrics.py
+++ web/tests/test_lib/test_metrics.py
@@ -5,14 +5,26 @@ import unittest
 
 class MetricsTest(unittest.TestCase):
 
+    # Default Config value is preserved
+    defaultConfigValue = metrics.CONF_FILE
+
     def test_correctInput(self):
+        metrics.CONF_FILE = MetricsTest.defaultConfigValue
         data = metrics.get('teammetrics','list')
         self.assertIsInstance(data,list)
         self.assertTrue(len(data)>0)
         self.assertIn('teammetrics-discuss', data)
 
     def test_incorrectInput(self):
+        metrics.CONF_FILE = MetricsTest.defaultConfigValue
         data = metrics.get('teammetrics','blah')
         self.assertEqual(len(data),0)
         data = metrics.get('blah','teammetrics')
+        self.assertEqual(len(data),0)
+
+    def test_configFileMissing(self):
+        metrics.CONF_FILE = 'SomeRandomFileThatDontExist'
+        data = metrics.get('teammetrics','list')
+        self.assertRaises(IOError)
+        self.assertIsInstance(data,list)
         self.assertEqual(len(data),0)
