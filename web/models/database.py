9f445db37b84927a0e57007a46c9247d76706963
Vipin Nair <swvist@gmail.com>
2012-06-21 07:12:11 +0530

    Added a password option to connect to the database

--- web/models/database.py
+++ web/models/database.py
@@ -13,6 +13,7 @@ def connect():
             conn = psycopg2.connect(host=settings.HOST,
                                     port=settings.DEFAULTPORT,
                                     user=settings.USER,
+                                    password=settings.PASS,
                                     database=settings.DB)
         except psycopg2.OperationalError:
             conn = psycopg2.connect(host=settings.HOSTIP,
