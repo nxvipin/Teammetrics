bf62cfc0951cad149baa551c24ef95fd63a87620
Vipin Nair <swvist@gmail.com>
2012-06-27 07:08:47 +0530

    Modified view function to work with decorators. Added some API error handling in the decorators.

--- web/api/views.py
+++ web/api/views.py
@@ -1,50 +1,68 @@
 from django.http import HttpResponse
-from web.lib import log
+from web.lib import log, lib
 from web.api import helper
 import json
 
 logger = log.get(__name__)
 
+@lib.respond('JSON')
+@lib.jsonify
+@lib.versionCheck
 def month(request, api_version, team, metric):
-    data = helper.getMonthData(api_version, team, metric)
+    data = helper.getMonthData(team, metric)
     data['team'] = team
-    return HttpResponse(json.dumps(data))
+    return data
 
+@lib.respond('JSON')
+@lib.jsonify
+@lib.versionCheck
 def monthAll(request, api_version, team):
     data = {}
     data['team'] = team
     data['data'] = []
-    data['data'].append(helper.getMonthData(api_version, team, 'list'))
-    data['data'].append(helper.getMonthData(api_version, team, 'commits'))
-    data['data'].append(helper.getMonthData(api_version, team, 'commitlines'))
-    return HttpResponse(json.dumps(data))
+    data['data'].append(helper.getMonthData(team, 'list'))
+    data['data'].append(helper.getMonthData(team, 'commits'))
+    data['data'].append(helper.getMonthData(team, 'commitlines'))
+    return data
 
+@lib.respond('JSON')
+@lib.jsonify
+@lib.versionCheck
 def monthTopN(request, api_version, team, metric, n):
     """
     Returns monthly data for top N members.
     """
-    data = helper.getMonthTopNData(api_version, team, metric, n)
+    data = helper.getMonthTopNData(team, metric, n)
     data['team'] = team
-    return HttpResponse(json.dumps(data))
+    return data
 
+@lib.respond('JSON')
+@lib.jsonify
+@lib.versionCheck
 def annual(request, api_version, team, metric):
-    data = helper.getAnnualData(api_version, team, metric)
+    data = helper.getAnnualData(team, metric)
     data['team'] = team
-    return HttpResponse(json.dumps(data))
+    return data
 
+@lib.respond('JSON')
+@lib.jsonify
+@lib.versionCheck
 def annualAll(request, api_version, team):
     data = {}
     data['team'] = team
     data['data'] = []
-    data['data'].append(helper.getAnnualData(api_version, team, 'list'))
-    data['data'].append(helper.getAnnualData(api_version, team, 'commits'))
-    data['data'].append(helper.getAnnualData(api_version, team, 'commitlines'))
-    return HttpResponse(json.dumps(data))
+    data['data'].append(helper.getAnnualData(team, 'list'))
+    data['data'].append(helper.getAnnualData(team, 'commits'))
+    data['data'].append(helper.getAnnualData(team, 'commitlines'))
+    return data
 
+@lib.respond('JSON')
+@lib.jsonify
+@lib.versionCheck
 def annualTopN(request, api_version, team, metric, n):
     """
     Returns monthly data for top N members.
     """
-    data = helper.getAnnualTopNData(api_version, team, metric, n)
+    data = helper.getAnnualTopNData(team, metric, n)
     data['team'] = team
-    return HttpResponse(json.dumps(data))
+    return data
