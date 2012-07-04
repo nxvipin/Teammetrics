7bc94795cc2ed470a45a5cb2c66b1b9f1e53037f
Vipin Nair <swvist@gmail.com>
2012-07-04 22:30:52 +0530

    Rearranged to fix rules

--- web/api/urls.py
+++ web/api/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^v(?P<api_version>\d+)/(?P<team>[a-zA-Z-]+)/$', 'api.views.monthAll'),
-    url(r'^v(?P<api_version>\d+)/(?P<team>[a-zA-Z-]+)/(?P<metric>[a-z]+)/$', 'api.views.month'),
-    url(r'^v(?P<api_version>\d+)/(?P<team>[a-zA-Z-]+)/(?P<metric>[a-z]+)/(?P<n>\d+)/$', 'api.views.monthTopN'),
     url(r'^v(?P<api_version>\d+)/a/(?P<team>[a-zA-Z-]+)/$', 'api.views.annualAll'),
     url(r'^v(?P<api_version>\d+)/a/(?P<team>[a-zA-Z-]+)/(?P<metric>[a-z]+)/$', 'api.views.annual'),
     url(r'^v(?P<api_version>\d+)/a/(?P<team>[a-zA-Z-]+)/(?P<metric>[a-z]+)/(?P<n>\d+)/$', 'api.views.annualTopN'),
+    url(r'^v(?P<api_version>\d+)/(?P<team>[a-zA-Z-]+)/$', 'api.views.monthAll'),
+    url(r'^v(?P<api_version>\d+)/(?P<team>[a-zA-Z-]+)/(?P<metric>[a-z]+)/$', 'api.views.month'),
+    url(r'^v(?P<api_version>\d+)/(?P<team>[a-zA-Z-]+)/(?P<metric>[a-z]+)/(?P<n>\d+)/$', 'api.views.monthTopN'),
 )
