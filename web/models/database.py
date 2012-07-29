c230e5581faaea70a2e4b3d9277c3e32ae286b52
Vipin Nair <swvist@gmail.com>
2012-07-30 00:44:24 +0530

    Changed function to accept the DB name as a paramter

--- web/models/database.py
+++ web/models/database.py
@@ -1,23 +1,23 @@
 import psycopg2
 import web.models.settings as settings
 
-def connect():
+def connect(db=settings.DB):
     """
     Connects to the database and returns a cursor.
     Default values are read from the settings file.
     """
     try:
-        conn = psycopg2.connect(database=settings.DB)
+        conn = psycopg2.connect(database=db)
     except psycopg2.OperationalError:   # pragma: no cover
         try:
             conn = psycopg2.connect(host=settings.HOST,
                                     port=settings.DEFAULTPORT,
                                     user=settings.USER,
                                     password=settings.PASS,
-                                    database=settings.DB)
+                                    database=db)
         except psycopg2.OperationalError:
             conn = psycopg2.connect(host=settings.HOSTIP,
                                     port=settings.PORT,
                                     user=settings.USER,
-                                    database=settings.DB)
+                                    database=db)
     return conn.cursor()
