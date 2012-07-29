871af0cc9fa83bc718cc2aa6dcbbbbe44aa109b0
Vipin Nair <swvist@gmail.com>
2012-07-30 03:58:45 +0530

    Added an API endpoint for dynamic rendering

--- web/ui/urls.py
+++ web/ui/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     url(r'^getdata$', 'ui.views.getData'),
     url(r'^$', 'ui.views.index'),
-    url(r'^(?P<team>[a-zA-Z-]+)/(?P<metric>[a-zA-Z-]+)/$', 'ui.views.teamdata')
+    url(r'^(?P<team>[a-zA-Z-]+)/(?P<metric>[a-zA-Z-]+)/$', 'ui.views.teamdata'),
+    url(r'^js/(?P<team>[a-zA-Z-]+)/(?P<metric>[a-zA-Z-]+)/$', 'ui.views.teamdatajs')
 )
