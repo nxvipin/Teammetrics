868abc28d77962c3b287c363b3a8530c4fd668ee
Vipin Nair <swvist@gmail.com>
2012-12-11 01:18:43 +0530

    Adding default settings file for the application

---
+++ web/default.settings.py
@@ -0,0 +1,25 @@
+"""
+Application Settings
+Edit this file and rename it to settings.py to configure the application
+"""
+
+#Application Settings
+PATH = '' #Application Root (Absolute Path)
+
+#Database Settings
+PORT = ''
+DEFAULTPORT = ''
+DB = ''
+USER = ''
+HOST = ''
+HOSTIP = ''
+PASS = ''
+
+#API Settings
+API_CURRENT_VERSION = '' #Current API Version
+API_SUPPORTED_VERSIONS = [] #Supported API versions
+
+#Configuration Settings
+CONFIG_FILE = {
+    'metrics' : '' #Metric Configuration File
+}
